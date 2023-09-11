4f8344f7dab8c673891417245ff39a7d9321ab69
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-09-11 17:01:26 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Layout from "./Pages/Layout.jsx";
 import HomePage from "./Pages/Home";
+import User from "./Pages/User";
 import NotFoundPage from "./Pages/404 Page/NotFoundPage.jsx";
 
 export default function App() {
@@ -11,6 +12,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
+          <Route path="/user/:id" component={User} />
           {/* This is the catch-all route */}
           <Route path="*" element={<NotFoundPage />} />
         </Route>
