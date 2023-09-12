e0b7520b8ad25666393dfe755cf6f01770773969
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-09-12 11:33:04 +0100

    Update User.jsx

--- src/Pages/User.jsx
+++ src/Pages/User.jsx
@@ -1,37 +1,70 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { motion } from "framer-motion";
-
-const animationConfiguration = {
-    initial: { opacity: 0 },
-    animate: { opacity: 1 },
-    exit: { opacity: 0 },
-};
 
 
 const User = props => {
     var id = props.match.params.id
-    return (
-        <motion.div
-            variants={animationConfiguration}
-            initial="initial"
-            animate="animate"
-            exit="exit"
-            transition={{ duration: 3 }}
-        >
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [user, setUser] = useState([]);
+    const [userAddress, setUserAddress] = useState([]);
+    const [userCompany, setUserCompany] = useState([]);
 
-            <section className='hero'>
-                <div className='hero-content'>
-                    <div className=''>
-                        <h1>User Details</h1>
-                        ID: {id}
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/users/" + id)
+            .then(res => res.json())
+            .then(
+                (data) => {
+                    console.log(data);
+                    setIsLoaded(true);
+                    setUser(data);
+                    setUserAddress(data.address);
+                    setUserCompany(data.company);
+                },
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            )
+    }, [])
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    }
+    if (!isLoaded) {
+        return <div>Loading...</div>;
+    }
 
+    if (user) {
+        return (
+            <motion.div
+                variants={animationConfiguration}
+                initial="initial"
+                animate="animate"
+                exit="exit"
+                transition={{ duration: 3 }}
+            >
+
+                <div>
+                    <h1>{user.name}</h1>
+                    <div>
+                        Email: {user.email}
                     </div>
-
+                    <div>
+                        Phone: {user.phone}
+                    </div>
+                    <div>
+                        Website: {user.website}
+                    </div>
+                    <div>
+                        Company: {userCompany.name}
+                    </div>
+                    <div>
+                        Address: {userAddress.street}, {userAddress.suite}, {userAddress.city}, {userAddress.zipcode}
+                    </div>
                 </div>
-            </section>
 
-
-        </motion.div>
-    );
+            </motion.div>
+        );
+    }
 }
 export default User;
