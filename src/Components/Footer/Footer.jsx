91b0d1165a1fab7f5fbe36e312f8dd4ed51ee989
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-09-20 15:26:45 +0100

    Update Footer.jsx

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -10,13 +10,28 @@ const Footer = () => {
             <footer
                 className="footer laptop:p-10 p-6 max-w-[90%] laptop:mx-[7rem] mx-0">
 
-                <p className="py-2 copyright laptop:mr-[rem] laptop:ml-0 mx-8 text-[0.85rem] laptop:block tablet:block ">
-                    {/* text-[#ffffff74] */}
-                    <span className="text-[0.85rem]">{theDate} </span>
-                    @
-                    Company.
-                    All rights reserved.
-                </p>
+                <div className="flex justify-between w-full gap-[rem]">
+                    <p className="py-2 copyright laptop:mr-[rem] laptop:ml-0 mx-8 text-[0.85rem] laptop:block tablet:block ">
+                        {/* text-[#ffffff74] */}
+                        Made with 💙
+
+                        by &nbsp;
+                        <a href="https://stephen-adeniji.netlify.app/"
+                            className="text-[0.85rem] text-blue-300 link link-hover">
+                            Stephen Adeniji
+                        </a>
+                    </p>
+                    <p className="py-2 copyright text-white/50 laptop:mr-[rem] laptop:ml-0 mx-8 text-[0.85rem] laptop:block tablet:block ">
+                        *API Consumption => (IMDB)*
+                    </p>
+                    <p className="py-2 copyright laptop:mr-[rem] laptop:ml-0 mx-8 text-[0.85rem] laptop:block tablet:block ">
+                        {/* text-[#ffffff74] */}
+                        <span className="text-[0.85rem]">{theDate} </span>
+                        @
+                        Company.
+                        All rights reserved.
+                    </p>
+                </div>
             </footer>
 
         </div>
