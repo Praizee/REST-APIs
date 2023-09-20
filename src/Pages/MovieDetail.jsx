8caa33d718eb88a41a236f7bf2e830089201b861
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-09-20 15:51:25 +0100

    Update MovieDetail.jsx

--- src/Pages/MovieDetail.jsx
+++ src/Pages/MovieDetail.jsx
@@ -4,20 +4,77 @@ const MovieDetail = ({ movie }) => {
   return (
     <div className="grid laptop:grid-cols-4 tablet:grid-cols-2 grid-cols-1 gap-4 p-4">
       <div className="text-left w-auto border p-2 rounded-2xl">
-        <img src={movie.Poster} alt={movie.Title} className="w-full mb-4 rounded-2xl" />
-        <h1>
+        <img
+          src={movie.Poster}
+          alt={movie.Title}
+          className="w-full mb-4 rounded-2xl"
+        />
+        <h1 className="">
           <b>Movie Title:</b> &nbsp;
           {movie.Title}
         </h1>
         <p>
-          <b>Year:</b> &nbsp;
+          <b>Year: </b> &nbsp;
           {movie.Year}
         </p>
         <p>
           <b>Released:</b> &nbsp;
           {movie.Released}
         </p>
-        {/* Add other movie details as needed */}
+        <p>
+          <b>Rated:</b> &nbsp;
+          {movie.Rated}
+        </p>
+        <p>
+          <b>Genre:</b> &nbsp;
+          {movie.Genre}
+        </p>
+        <p>
+          <b>Writer:</b> &nbsp;
+          {movie.Writer}
+        </p>
+        <p>
+          <b>Actors:</b> &nbsp;
+          {movie.Actors}
+        </p>
+        <p>
+          <b>Plot:</b> &nbsp;
+          {movie.Plot}
+        </p>
+        <p>
+          <b>Type:</b> &nbsp;
+          {movie.Type}
+        </p>
+        <p>
+          <b>Runtime:</b> &nbsp;
+          {movie.Runtime}
+        </p>
+        <p>
+          <b>Production:</b> &nbsp;
+          {movie.Production}
+        </p>
+        <p>
+          <b>IMDB Rating:</b> &nbsp;
+          {movie.imdbRating}
+        </p>
+        <p>
+          <b>IMDB ID:</b> &nbsp;
+          {movie.imdbID}
+        </p>
+        <p>
+          <b>Country:</b> &nbsp;
+          {movie.Country}
+        </p>
+        <p>
+          <b>Languages:</b> &nbsp;
+          {movie.Language}
+        </p>
+        <p>
+          <b>Awards:</b> &nbsp;
+          {movie.Awards}
+        </p>
+        {/* <p>Ratings: [{movie.Ratings}]</p> */}
+        <p className='hidden'>{movie.Response}</p>
       </div>
     </div>
   );
