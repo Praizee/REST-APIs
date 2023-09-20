eac0526061812ada59e7a2a8ed6a5dc2b39cd91f
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-09-20 15:15:02 +0100

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
+import MovieDetail from './MovieDetail';
 
 const animationConfiguration = {
   initial: { opacity: 0 },
@@ -12,6 +13,7 @@ const Home = () => {
   const [search, setSearch] = useState('');
   const [yearFilter, setYearFilter] = useState('');
   const [imdbIdFilter, setImdbIdFilter] = useState('');
+  const [selectedMovie, setSelectedMovie] = useState(null); // Track the selected movie
   const API_KEY = 'e6a9241d';
   const searchUrl = `http://www.omdbapi.com/?s=${search}&y=${yearFilter}&i=${imdbIdFilter}&apikey=${API_KEY}`;
   const popularMoviesUrl = `http://www.omdbapi.com/?s=popular&apikey=${API_KEY}`;
@@ -21,7 +23,15 @@ const Home = () => {
       const response = await fetch(searchUrl);
       const data = await response.json();
       if (data.Search) {
-        setMovies(data.Search);
+        let sortedMovies = [...data.Search]; // Create a copy of the movies array to avoid modifying the original data
+        if (yearFilter === 'recent') {
+          // Sort by year in descending order (most recent first)
+          sortedMovies = sortedMovies.sort((a, b) => parseInt(b.Year) - parseInt(a.Year));
+        } else if (yearFilter === 'oldest') {
+          // Sort by year in ascending order (oldest first)
+          sortedMovies = sortedMovies.sort((a, b) => parseInt(a.Year) - parseInt(b.Year));
+        }
+        setMovies(sortedMovies);
       } else {
         setMovies([]);
       }
@@ -30,16 +40,27 @@ const Home = () => {
     }
   };
 
+
   const onInputChange = (e) => {
     setSearch(e.target.value);
   };
 
   const onYearFilterChange = (e) => {
     setYearFilter(e.target.value);
+    getMovies(); // Trigger sorting when the filter option changes
   };
+
 
   const onImdbIdFilterChange = (e) => {
     setImdbIdFilter(e.target.value);
+  };
+
+  const handleCancel = () => {
+    setSearch('');
+    setYearFilter('');
+    setImdbIdFilter('');
+    setMovies([]);
+    setSelectedMovie(null); // Reset selected movie
   };
 
   useEffect(() => {
@@ -63,60 +84,81 @@ const Home = () => {
           <div className="mx-[rem] min-h-[calc(100vh-13.5rem)]">
             <div className="mx-4 text-center">
               <div className="my-12">
-                {/* input */}
-                <input
-                  type="text"
-                  value={search}
-                  onChange={onInputChange}
-                  autoComplete="true"
-                  className="laptop:text-[1.3rem] text-[1rem] tablet:text-[1.2rem] p-2 px-4 pl-5 bg-slate-200 rounded-l-3xl"
-                  placeholder="Search for movies..."
-                />
-                {/* Search button */}
-                <button
-                  type="submit"
-                  onClick={getMovies}
-                  className="btn laptop:text-[1.3rem] text-[1rem] tablet:text-[1.2rem] p-2 px-4 normal-case text-[#f8f8fd] bg-[#DF8026] hover:bg-[#DF8026]/75 rounded-r-3xl rounded-l-none border-none"
-                >
-                  Search
-                </button>
+                <div className="my-12">
+                  {/* Search input */}
+                  <input
+                    type="text"
+                    value={search}
+                    onChange={onInputChange}
+                    autoComplete="true"
+                    placeholder="Search for movies..."
+                    className="laptop:text-[1.3rem] text-[1rem] tablet:text-[1.2rem] p-2 px-4 pl-5 bg-slate-200 rounded-l-3xl"
+                  />
+                  {/* Search button */}
+                  <button
+                    type="submit"
+                    onClick={getMovies}
+                    className="btn laptop:text-[1.3rem] text-[1rem] tablet:text-[1.2rem] p-2 px-4 normal-case text-[#f8f8fd] bg-[#DF8026] hover:bg-[#DF8026]/75 rounded-r-3xl rounded-l-none border-none"
+                  >
+                    Search
+                  </button>
+                </div>
 
-                {/* Filters */}
+                {/* Filters and Cancel button */}
                 <div className="mt-4 flex space-x-4">
                   <input
                     type="text"
                     value={yearFilter}
                     onChange={onYearFilterChange}
-                    className="p-2 px-4 bg-slate-200 rounded-md"
                     placeholder="Year filter"
+                    className="p-2 px-4 bg-slate-200 rounded-md"
                   />
                   <input
                     type="text"
                     value={imdbIdFilter}
                     onChange={onImdbIdFilterChange}
-                    className="p-2 px-4 bg-slate-200 rounded-md"
                     placeholder="IMDb ID filter"
+                    className="p-2 px-4 bg-slate-200 rounded-md"
                   />
+                  {/* Year filter dropdown */}
+                  <select
+                    value={yearFilter}
+                    onChange={onYearFilterChange}
+                    className="p-2 px-4 bg-slate-200 rounded-md"
+                  >
+                    <option value="">Select Year</option>
+                    <option value="recent">Recent First</option>
+                    <option value="oldest">Oldest First</option>
+                  </select>
+
+                  <button
+                    type="button"
+                    onClick={handleCancel}
+                    className="btn laptop:text-[1.3rem] text-[1rem] tablet:text-[1.2rem] p-2 px-4 normal-case text-[#f8f8fd] bg-red-500 hover:bg-red-600 rounded-md"
+                  >
+                    Cancel
+                  </button>
                 </div>
-
-
               </div>
 
               {/* Movie results */}
               <div className="">
-                {movies.length > 0 ? (
+                {selectedMovie ? ( // If a movie is selected, show the detailed view
+                  <MovieDetail movie={selectedMovie} />
+                ) : movies.length > 0 ? (
                   <div className="grid laptop:grid-cols-4 tablet:grid-cols-2 grid-cols-1 gap-4 p-4">
                     {movies.map((movie) => (
                       <div
                         key={movie.imdbID}
                         className="text-left w-auto border p-2 rounded-2xl"
+                        onClick={() => setSelectedMovie(movie)} // Set the selected movie on click
                       >
                         <img
                           src={movie.Poster}
                           alt={movie.Title}
                           className="w-full mb-4 rounded-2xl"
                         />
-                        <h1 className="">
+                        <h1>
                           <b>Movie Title:</b> &nbsp;
                           {movie.Title}
                         </h1>
