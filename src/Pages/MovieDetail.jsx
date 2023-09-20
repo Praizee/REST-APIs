6bbd863ec1d5c7d1016325312abf77bc5ca56aea
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-09-20 16:48:42 +0100

    Update MovieDetail.jsx
    displays only available properties

--- src/Pages/MovieDetail.jsx
+++ src/Pages/MovieDetail.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 const MovieDetail = ({ movie }) => {
+  console.log(movie); // Log the movie object to the console
+
   return (
     <div className="grid laptop:grid-cols-4 tablet:grid-cols-2 grid-cols-1 gap-4 p-4">
       <div className="text-left w-auto border p-2 rounded-2xl">
@@ -14,45 +16,57 @@ const MovieDetail = ({ movie }) => {
           {movie.Title}
         </h1>
         <p>
-          <b>Year: </b> &nbsp;
+          <b>Year:</b> &nbsp;
           {movie.Year}
         </p>
         <p>
           <b>Released:</b> &nbsp;
           {movie.Released}
         </p>
-        <p>
-          <b>Rated:</b> &nbsp;
-          {movie.Rated}
-        </p>
+        {movie.Rated && (
+          <p>
+            <b>Rated:</b> &nbsp;
+            {movie.Rated}
+          </p>
+        )}
         <p>
           <b>Genre:</b> &nbsp;
           {movie.Genre}
         </p>
-        <p>
-          <b>Writer:</b> &nbsp;
-          {movie.Writer}
-        </p>
-        <p>
-          <b>Actors:</b> &nbsp;
-          {movie.Actors}
-        </p>
-        <p>
-          <b>Plot:</b> &nbsp;
-          {movie.Plot}
-        </p>
+        {movie.Writer && (
+          <p>
+            <b>Writer:</b> &nbsp;
+            {movie.Writer}
+          </p>
+        )}
+        {movie.Actors && (
+          <p>
+            <b>Actors:</b> &nbsp;
+            {movie.Actors}
+          </p>
+        )}
+        {movie.Plot && (
+          <p>
+            <b>Plot:</b> &nbsp;
+            {movie.Plot}
+          </p>
+        )}
         <p>
           <b>Type:</b> &nbsp;
           {movie.Type}
         </p>
-        <p>
-          <b>Runtime:</b> &nbsp;
-          {movie.Runtime}
-        </p>
-        <p>
-          <b>Production:</b> &nbsp;
-          {movie.Production}
-        </p>
+        {movie.Runtime && (
+          <p>
+            <b>Runtime:</b> &nbsp;
+            {movie.Runtime}
+          </p>
+        )}
+        {movie.Production && (
+          <p>
+            <b>Production:</b> &nbsp;
+            {movie.Production}
+          </p>
+        )}
         <p>
           <b>IMDB Rating:</b> &nbsp;
           {movie.imdbRating}
@@ -61,18 +75,24 @@ const MovieDetail = ({ movie }) => {
           <b>IMDB ID:</b> &nbsp;
           {movie.imdbID}
         </p>
-        <p>
-          <b>Country:</b> &nbsp;
-          {movie.Country}
-        </p>
-        <p>
-          <b>Languages:</b> &nbsp;
-          {movie.Language}
-        </p>
-        <p>
-          <b>Awards:</b> &nbsp;
-          {movie.Awards}
-        </p>
+        {movie.Country && (
+          <p>
+            <b>Country:</b> &nbsp;
+            {movie.Country}
+          </p>
+        )}
+        {movie.Language && (
+          <p>
+            <b>Languages:</b> &nbsp;
+            {movie.Language}
+          </p>
+        )}
+        {movie.Awards && (
+          <p>
+            <b>Awards:</b> &nbsp;
+            {movie.Awards}
+          </p>
+        )}
         {/* <p>Ratings: [{movie.Ratings}]</p> */}
         <p className='hidden'>{movie.Response}</p>
       </div>
@@ -81,3 +101,88 @@ const MovieDetail = ({ movie }) => {
 };
 
 export default MovieDetail;
+
+
+// import React from 'react';
+
+// const MovieDetail = ({ movie }) => {
+//   return (
+//     <div className="grid laptop:grid-cols-4 tablet:grid-cols-2 grid-cols-1 gap-4 p-4">
+//       <div className="text-left w-auto border p-2 rounded-2xl">
+//         <img
+//           src={movie.Poster}
+//           alt={movie.Title}
+//           className="w-full mb-4 rounded-2xl"
+//         />
+//         <h1 className="">
+//           <b>Movie Title:</b> &nbsp;
+//           {movie.Title}
+//         </h1>
+//         <p>
+//           <b>Year: </b> &nbsp;
+//           {movie.Year}
+//         </p>
+//         <p>
+//           <b>Released:</b> &nbsp;
+//           {movie.Released}
+//         </p>
+//         <p>
+//           <b>Rated:</b> &nbsp;
+//           {movie.Rated}
+//         </p>
+//         <p>
+//           <b>Genre:</b> &nbsp;
+//           {movie.Genre}
+//         </p>
+//         <p>
+//           <b>Writer:</b> &nbsp;
+//           {movie.Writer}
+//         </p>
+//         <p>
+//           <b>Actors:</b> &nbsp;
+//           {movie.Actors}
+//         </p>
+//         <p>
+//           <b>Plot:</b> &nbsp;
+//           {movie.Plot}
+//         </p>
+//         <p>
+//           <b>Type:</b> &nbsp;
+//           {movie.Type}
+//         </p>
+//         <p>
+//           <b>Runtime:</b> &nbsp;
+//           {movie.Runtime}
+//         </p>
+//         <p>
+//           <b>Production:</b> &nbsp;
+//           {movie.Production}
+//         </p>
+//         <p>
+//           <b>IMDB Rating:</b> &nbsp;
+//           {movie.imdbRating}
+//         </p>
+//         <p>
+//           <b>IMDB ID:</b> &nbsp;
+//           {movie.imdbID}
+//         </p>
+//         <p>
+//           <b>Country:</b> &nbsp;
+//           {movie.Country}
+//         </p>
+//         <p>
+//           <b>Languages:</b> &nbsp;
+//           {movie.Language}
+//         </p>
+//         <p>
+//           <b>Awards:</b> &nbsp;
+//           {movie.Awards}
+//         </p>
+//         {/* <p>Ratings: [{movie.Ratings}]</p> */}
+//         <p className='hidden'>{movie.Response}</p>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default MovieDetail;
