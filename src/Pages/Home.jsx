306b64029bfd8751df4ed4d656b39b282ee6f70a
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-09-20 14:38:24 +0100

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { motion } from "framer-motion";
-
+import { motion } from 'framer-motion';
 
 const animationConfiguration = {
   initial: { opacity: 0 },
@@ -10,117 +8,47 @@ const animationConfiguration = {
 };
 
 const Home = () => {
-  const [movie, setMovie] = useState({});
+  const [movies, setMovies] = useState([]);
   const [search, setSearch] = useState('');
-  const API_KEY = "e6a9241d";
-  const url = `http://www.omdbapi.com/?t=${search}&apikey=${API_KEY}`;
-  // const url = `http://www.omdbapi.com/?t=${search}&apikey=${API_KEY}&plot=full`;
+  const [yearFilter, setYearFilter] = useState('');
+  const [imdbIdFilter, setImdbIdFilter] = useState('');
+  const API_KEY = 'e6a9241d';
+  const searchUrl = `http://www.omdbapi.com/?s=${search}&y=${yearFilter}&i=${imdbIdFilter}&apikey=${API_KEY}`;
+  const popularMoviesUrl = `http://www.omdbapi.com/?s=popular&apikey=${API_KEY}`;
 
-  const getMovie = async () => {
+  const getMovies = async () => {
     try {
-      const response = await fetch(url);
-      const data = await response.json()
-      setMovie(data);
+      const response = await fetch(searchUrl);
+      const data = await response.json();
+      if (data.Search) {
+        setMovies(data.Search);
+      } else {
+        setMovies([]);
+      }
     } catch (error) {
       console.error(error);
     }
-  }
+  };
 
-  const onInputChange = e => {
+  const onInputChange = (e) => {
     setSearch(e.target.value);
-  }
+  };
+
+  const onYearFilterChange = (e) => {
+    setYearFilter(e.target.value);
+  };
+
+  const onImdbIdFilterChange = (e) => {
+    setImdbIdFilter(e.target.value);
+  };
 
   useEffect(() => {
-    getMovie();
-  }, []);
-
-
-  function checkResponse(data) {
-    if (data.Response === "True") {
-      return (
-        <div className="grid laptop:grid-cols-4 tablet:grid-cols-2 grid-cols-1 gap-4 p-4">
-          <div className="text-left w-auto border p-2 rounded-2xl">
-            <img
-              src={data.Poster}
-              alt={data.Title}
-              className="w-full mb-4 rounded-2xl"
-            />
-            <h1 className="">
-              <b>Movie Title:</b> &nbsp;
-              {data.Title}
-            </h1>
-            <p>
-              <b>Year: </b> &nbsp;
-              {data.Year}
-            </p>
-            <p>
-              <b>Released:</b> &nbsp;
-              {data.Released}
-            </p>
-            <p>
-              <b>Rated:</b> &nbsp;
-              {data.Rated}
-            </p>
-            <p>
-              <b>Genre:</b> &nbsp;
-              {data.Genre}
-            </p>
-            <p>
-              <b>Writer:</b> &nbsp;
-              {data.Writer}
-            </p>
-            <p>
-              <b>Actors:</b> &nbsp;
-              {data.Actors}
-            </p>
-            <p>
-              <b>Plot:</b> &nbsp;
-              {data.Plot}
-            </p>
-            <p>
-              <b>Type:</b> &nbsp;
-              {data.Type}
-            </p>
-            <p>
-              <b>Runtime:</b> &nbsp;
-              {data.Runtime}
-            </p>
-            <p>
-              <b>Production:</b> &nbsp;
-              {data.Production}
-            </p>
-            <p>
-              <b>IMDB Rating:</b> &nbsp;
-              {data.imdbRating}
-            </p>
-            <p>
-              <b>IMDB ID:</b> &nbsp;
-              {data.imdbID}
-            </p>
-            <p>
-              <b>Country:</b> &nbsp;
-              {data.Country}
-            </p>
-            <p>
-              <b>Languages:</b> &nbsp;
-              {data.Language}
-            </p>
-            <p>
-              <b>Awards:</b> &nbsp;
-              {data.Awards}
-            </p>
-            {/* <p>Ratings: [{data.Ratings}]</p> */}
-            <p className='hidden'>{data.Response}</p>
-          </div>
-        </div>
-      );
+    if (search || yearFilter || imdbIdFilter) {
+      getMovies();
+    } else {
+      setMovies([]);
     }
-    return (
-      <p className='my-8'>
-        No Movie found! 😥
-      </p>
-    );
-  }
+  }, [search, yearFilter, imdbIdFilter]);
 
   return (
     <motion.div
@@ -130,48 +58,90 @@ const Home = () => {
       exit="exit"
       transition={{ duration: 3 }}
     >
-      <section className='bg-[#F8F8FD] selection:bg-blue-400 border-2 border-blue-500 mt-[4rem] text-black'>
-
+      <section className="bg-[#F8F8FD] selection:bg-blue-400 border-2 border-blue-500 mt-[4rem] text-black">
         <div className="">
-          <div className='mx-[rem] min-h-[calc(100vh-13.5rem)]'>
-
-            <div className='mx-4 text-center'>
+          <div className="mx-[rem] min-h-[calc(100vh-13.5rem)]">
+            <div className="mx-4 text-center">
               <div className="my-12">
                 {/* input */}
-                <input type="text"
+                <input
+                  type="text"
                   value={search}
                   onChange={onInputChange}
-                  autoComplete='true'
+                  autoComplete="true"
                   className="laptop:text-[1.3rem] text-[1rem] tablet:text-[1.2rem] p-2 px-4 pl-5 bg-slate-200 rounded-l-3xl"
+                  placeholder="Search for movies..."
                 />
-
-                {/* search button */}
+                {/* Search button */}
                 <button
                   type="submit"
-                  onClick={getMovie}
+                  onClick={getMovies}
                   className="btn laptop:text-[1.3rem] text-[1rem] tablet:text-[1.2rem] p-2 px-4 normal-case text-[#f8f8fd] bg-[#DF8026] hover:bg-[#DF8026]/75 rounded-r-3xl rounded-l-none border-none"
                 >
                   Search
                 </button>
+
+                {/* Filters */}
+                <div className="mt-4 flex space-x-4">
+                  <input
+                    type="text"
+                    value={yearFilter}
+                    onChange={onYearFilterChange}
+                    className="p-2 px-4 bg-slate-200 rounded-md"
+                    placeholder="Year filter"
+                  />
+                  <input
+                    type="text"
+                    value={imdbIdFilter}
+                    onChange={onImdbIdFilterChange}
+                    className="p-2 px-4 bg-slate-200 rounded-md"
+                    placeholder="IMDb ID filter"
+                  />
+                </div>
+
+
               </div>
 
-
-              {/* movie search details/results */}
+              {/* Movie results */}
               <div className="">
-                {checkResponse(movie)}
+                {movies.length > 0 ? (
+                  <div className="grid laptop:grid-cols-4 tablet:grid-cols-2 grid-cols-1 gap-4 p-4">
+                    {movies.map((movie) => (
+                      <div
+                        key={movie.imdbID}
+                        className="text-left w-auto border p-2 rounded-2xl"
+                      >
+                        <img
+                          src={movie.Poster}
+                          alt={movie.Title}
+                          className="w-full mb-4 rounded-2xl"
+                        />
+                        <h1 className="">
+                          <b>Movie Title:</b> &nbsp;
+                          {movie.Title}
+                        </h1>
+                        <p>
+                          <b>Year:</b> &nbsp;
+                          {movie.Year}
+                        </p>
+                        {/* Add other movie details as needed */}
+                      </div>
+                    ))}
+                  </div>
+                ) : (
+                  <p className="my-8">
+                    {search || yearFilter || imdbIdFilter
+                      ? 'No matching movies found! 😥'
+                      : 'Start your search above.'}
+                  </p>
+                )}
               </div>
-
             </div>
-
           </div>
         </div>
-
       </section>
-
-
     </motion.div>
   );
-}
-
+};
 
 export default Home;
