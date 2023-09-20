f5e724c215a6adfba393b0f5c53a50bd7d0f5cfd
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-09-20 16:22:53 +0100

    Update Footer.jsx

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
             <footer
                 className="footer laptop:p-10 p-6 max-w-[90%] laptop:mx-[7rem] mx-0">
 
-                <div className="flex justify-between w-full gap-[rem]">
+                <div className="laptop:flex text-center justify-between w-full gap-[rem]">
                     <p className="py-2 copyright laptop:mr-[rem] laptop:ml-0 mx-8 text-[0.85rem] laptop:block tablet:block ">
                         {/* text-[#ffffff74] */}
                         Made with 💙
